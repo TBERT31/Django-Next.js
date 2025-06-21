74d2e344f973d9e7a7834e9f5cb8d6d5db00d580
BERTEAU Thomas <thomas.berteau@egis-group.com>
2025-06-21 21:42:12 +0200

    Add jwt protection

--- django-nextjs-backend-api/src/cfehome/api.py
+++ django-nextjs-backend-api/src/cfehome/api.py
@@ -1,19 +1,23 @@
 from ninja import NinjaAPI, Schema
 
-api = NinjaAPI()
+from ninja_extra import NinjaExtraAPI
+from ninja_jwt.authentication import JWTAuth
+from ninja_jwt.controller import NinjaJWTDefaultController
+
+api = NinjaExtraAPI()
+api.register_controllers(NinjaJWTDefaultController)
 
 class UserSchema(Schema):
     username: str
     is_authenticated: bool
     # is not requst.user.is_authenticated
-    # email: str = None
+    email: str = None
 
 @api.get("/hello")
 def hello(request):
     print(request)
     return "Hello World"
 
-@api.get("/me", 
-    response=UserSchema)
+@api.get("/me", response=UserSchema, auth=JWTAuth())
 def me(request):
     return request.user
