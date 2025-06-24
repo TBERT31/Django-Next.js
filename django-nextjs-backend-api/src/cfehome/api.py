27158f898c83e665a81012720ae47c44fae712ad
BERTEAU Thomas <thomas.berteau@egis-group.com>
2025-06-24 10:27:59 +0200

    Setup env

--- django-nextjs-backend-api/src/cfehome/api.py
+++ django-nextjs-backend-api/src/cfehome/api.py
@@ -1,4 +1,5 @@
 from ninja import NinjaAPI, Schema
+import helpers
 
 from ninja_extra import NinjaExtraAPI
 from ninja_jwt.authentication import JWTAuth
@@ -21,6 +22,6 @@ def hello(request):
 
 @api.get("/me", 
          response=UserSchema, 
-         auth=JWTAuth())
+         auth=helpers.api_auth_user_required)
 def me(request):
     return request.user
--- django-nextjs-backend-api/src/waitlists/schemas.py
+++ django-nextjs-backend-api/src/waitlists/schemas.py
@@ -1,3 +1,4 @@
+from typing import List, Any, Optional
 from datetime import datetime
 from ninja import Schema
 from pydantic import EmailStr
@@ -8,11 +9,21 @@ class WaitlistEntryCreateSchema(Schema):
     # WaitlistEntryIn
     email: EmailStr
 
+class ErrorWaitlistEntryCreateSchema(Schema):
+    # Create -> Data
+    # WaitlistEntryIn
+    email: List[Any]
+    # non_field_errors: List[dict] = []
+
 class WaitlistEntryListSchema(Schema):
     # List -> Data
     # WaitlistEntryOut
     id: int
     email: EmailStr
+    updated: datetime
+    timestamp: datetime
+    description: Optional[str] = ""
+    
 
 class WaitlistEntryDetailSchema(Schema):
     # Get -> Data
